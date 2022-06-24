326e15adc954fd1d5d02c43036a81a8ee34a3531
souravsingh7 <souravsingh2000I@gmail.com>
2022-06-24 11:54:21 +0530

    push again

--- js/client.js
+++ js/client.js
@@ -4,7 +4,8 @@ const socket = io("http://localhost:8000");
 const form = document.getElementById("send-container");
 const messageInput = document.getElementById("messageInp");
 const messageContainer = document.querySelector(".container");
-var audio= new Audio('ting.mp3')
+
+var audio = new Audio("bg.mp3");
 
 // Function which will append event info to the contaner
 const append = (message, position) => {
@@ -13,9 +14,9 @@ const append = (message, position) => {
   messageElement.classList.add('message');
   messageElement.classList.add(position);
   messageContainer.append(messageElement);
-if(position=='left'){
-  audio.play();
-}
+  if (position == 'left') {
+    audio.play();
+  }
 }
 
 form.addEventListener('submit',(e)=>{
